cd5d2e73dd5cad36e0f53ee48c36fcba9d68035b
Abu Salam (Latitude E5450) <abusalamparvezalam@gmail.com>
2016-03-05 16:00:30 +0530

    First Randomization of PhaseOne

--- fpdf/training-app-letter3.php
+++ fpdf/training-app-letter3.php
@@ -76,19 +76,19 @@ function FancyTable($header, $data)
 			
 			$euname="ELECTION URGENT";
 			$euname1="ORDER OF APPOINTMENT FOR TRAINING";
-			$euname2="ASSEMBLY GENERAL ELECTION , 2016";
+			$euname2="General Election to West Bengal Legislative Assembly 2016";
 			$euname3="Token No. ".$row['token'];
 			$euname4="Order No: ".$_SESSION['apt1_orderno'];
 			$euname5="Date: ".$_SESSION['apt1_date'];
 			$euname6="In exercise of the power conferred upon vide Section 26 of the R. P. Act, 1951, I do hereby appoint the officer specified below";
-			$euname7="as ".$row['poststatus']." for undergoing training in connection with the conduct of Assembly General Election, 2016.";
+			$euname7="as ".$row['poststatus']." for undergoing training in connection with the conduct of General Election to West Bengal Legislative Assembly 2016.";
 			//$euname8="$row[forpc]-$row[pcname] PC";			
 			$euname9="The Officer should report for Training as per following Schedule.";
 			$euname10="This is a compulsory duty on your part to attend the said programme, as per the provisions of The Representation of the People Act, 1951.";
 			$euname11="You are directed to bring your Elector's Photo Identity Card (EPIC) or any proof of Identity affixed with your Photograph.";
 			$euname12="Place: ".uppercase($_SESSION['distnm_cap']);
 			$euname13="Date: ".date("d/m/Y");
-			$euname14="District ".wordcase($_SESSION['distnm_cap']);
+			$euname14="".wordcase($_SESSION['distnm_cap']);
 			$nb1="Please fillup Form 12 (for postal ballot) annexed herewith and submit at the Training Venue on the 1st day of training along with";
 			$nb2="the duplicate copy of your appointment letter and a copy of EPIC or any other ID including service ID.";
 			$nb3="Please indicate your PIN Number as given in your appointment letter on the body of Form 12 to help us locate you for delivery of";
@@ -120,7 +120,7 @@ function FancyTable($header, $data)
 			$this->Cell(15,10,$euname4,0,0,'L');
 			$this->SetFont('Arial','B',7);
 			$this->Cell(70);
-			$this->Cell(40,8,$euname2,0,0,'C');
+			$this->Cell(30,8,$euname2,0,0,'C');
 			$this->SetFont('Arial','B',8);
 			$this->Cell(50);
 			$this->Cell(10,10,$euname5,0,0,'R');
@@ -261,7 +261,7 @@ function FancyTable($header, $data)
 			
 			$this->SetFont('Arial','',9);
 			$this->Cell(160);
-			$this->Cell(10,10,"District Election Officer",0,0,'R');
+			$this->Cell(6,10,"District Election Officer",0,0,'R');
 		
 			// Line break
 			$this->Ln(4);
