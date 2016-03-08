929c7f33be374890f7e4d805aa270d2ea4ceef1a
Souvik De <souvik12.de@gmail.com>
2016-03-08 20:36:56 +0530

    Added Sl no in VenueList and pagebreak on 50 records

--- fpdf/venue-wise-attendance.php
+++ fpdf/venue-wise-attendance.php
@@ -105,6 +105,7 @@ function FancyTable($header, $data)
     $fill = false;
 	$count=0;
 	$per_page=1;
+	$counter=0;
 
 	//$signature="../images/deo/img.jpg";
     for($i=1;$i<=rowCount($data);$i++)
@@ -139,16 +140,16 @@ function FancyTable($header, $data)
 			$this->SetLineWidth(.3);
 			$this->SetFont('','B');
 			
-			$head = array('PIN','Name','Designation','Posting Status','Enrollment Details','Token No','Signature');
-			$header1 = array('','','','','AC / Part /Sl No.','','');
-			$w = array(15,40,50,22,25,18,25);
+			$head = array('SlNo','PIN','Name','Designation','Posting Status','Enrollment Details','Token No','Signature');
+			$header1 = array('','','','','','AC / Part /Sl No.','','');
+			$w = array(10,15,40,40,22,25,18,25);
 				$this->SetFont('Arial','',8);
 			for($j=0;$j<count($head);$j++)
 				$this->Cell($w[$j],7,$head[$j],'LTR',0,'C',true);
 			$this->Ln();
 			for($l=0;$l<count($header1);$l++)
 				$this->Cell($w[$l],4,$header1[$l],'LR',0,'C',true);
-			
+			$counter=0;
 			$this->Ln();
 		//	$this->Cell(10,5,$subdiv,0,0,'L');
 		   
@@ -157,29 +158,40 @@ function FancyTable($header, $data)
 			
 			for($k=1;$k<=rowCount($rsPersonnel);$k++)
 	        {
+			$counter=$counter+1;
+			
+			
+				
 				$rowPersonnel=getRows($rsPersonnel);
 			//	$sql="";
 				$this->SetFont('','',5.2);
                    
 				  $totaljoin=$rowPersonnel['acno']." / ".$rowPersonnel['partno']." / ".$rowPersonnel['slno'];
 				  $token_join=$rowPersonnel['forsubdivision']." / ".$rowPersonnel['poststat']." / ".$rowPersonnel['token'];
-				  
-					$this->Cell($w[0],6,$rowPersonnel['personcd'],'LTR',0,'L',$fill);						
-					$this->Cell($w[1],6,$rowPersonnel['officer_name'],'LTR',0,'L',$fill);
-					$this->Cell($w[2],6,$rowPersonnel['designation'],'LTR',0,'L',$fill);
-					$this->Cell($w[3],6,$rowPersonnel['poststatus'],'LTR',0,'L',$fill);
-					$this->Cell($w[4],6,$totaljoin,'LTR',0,'L',$fill);
-					$this->Cell($w[5],6,$token_join,'LTR',0,'L',$fill);
-					$this->Cell($w[6],6,'','LTR',0,'L',$fill);
+				    $this->Cell($w[0],6,$counter,'LTR',0,'L',$fill);
+					$this->Cell($w[1],6,$rowPersonnel['personcd'],'LTR',0,'L',$fill);						
+					$this->Cell($w[2],6,$rowPersonnel['officer_name'],'LTR',0,'L',$fill);
+					$this->Cell($w[3],6,$rowPersonnel['designation'],'LTR',0,'L',$fill);
+					$this->Cell($w[4],6,$rowPersonnel['poststatus'],'LTR',0,'L',$fill);
+					$this->Cell($w[5],6,$totaljoin,'LTR',0,'L',$fill);
+					$this->Cell($w[6],6,$token_join,'LTR',0,'L',$fill);
+					$this->Cell($w[7],6,'','LTR',0,'L',$fill);
 					//count1++;
-					
 					$this->Ln();
+					$r=fmod($counter,50);
+					if($r==0)
+					{   
+						$this->AddPage();
+						$this->Ln();
+					}
+					
 					$this->Cell(array_sum($w),0,'',1,0,'L',$fill);
 					$this->Ln();
 					
 			
-				
+			
 			}
+			
 			
 			$fill = !$fill;
 		    $count++;
